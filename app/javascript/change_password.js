b0558d75ca2edfa3f949777903337545b75908df
kyota1210 <kytmbmp1210@gmail.com>
2020-10-20 12:35:20 +0900

    エラーメッセージの表示実装

---
+++ app/javascript/change_password.js
@@ -0,0 +1,21 @@
+function pullDown() {
+  const pullDownButton = document.getElementById("label1")
+  const pullDownShow = document.getElementById("pulldown-show")
+
+  pullDownButton.addEventListener('mouseover', function(){
+    this.setAttribute("style", "background-color:#1e2c41;")
+  })
+  pullDownButton.addEventListener('mouseout', function(){
+    this.removeAttribute("style", "background-color:#1e2c41;")
+  })
+
+  pullDownButton.addEventListener('click', function() {
+    if (pullDownShow.getAttribute("style") == "display:block;") {
+      pullDownShow.removeAttribute("style", "display:block;")
+    } else {
+      pullDownShow.setAttribute("style", "display:block;")
+    }
+  })
+}
+
+window.addEventListener('load', pullDown)
