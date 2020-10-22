f12eef37c83add965922501bf7b60dedbc3c6d04
kyota1210 <kytmbmp1210@gmail.com>
2020-10-22 19:32:44 +0900

    ユーザー編集機能実装

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  # before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_account_update_params, only: [:update]
+
+
+  protected
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+end
