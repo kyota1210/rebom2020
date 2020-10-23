638026c9466748bd173a0bec0a38b507bd609397
kyota1210 <kytmbmp1210@gmail.com>
2020-10-23 18:42:17 +0900

    postモデルのバリデーション設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,9 @@ class Book < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_one_attached :image
 
-  validates :title,  presence: true
-  validates :author, presence: true
-  validates :image, presence: true
+  with_options presence: true do
+    validates :title,  length: { maximum: 36, message: 'は36字以内で入力してください' }
+    validates :author, length: { maximum: 23, message: 'は23字以内で入力してください' }
+    validates :image
+  end
 end
