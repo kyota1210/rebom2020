67bb5ad6e484758f9d1dfad5809c1e7ed327c453
kyota1210 <kytmbmp1210@gmail.com>
2020-10-24 18:26:15 +0900

    ユーザーテスト画像の添付

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
     email                 { Faker::Internet.free_email }
     password              { '123abc' }
     password_confirmation { password }
+
+    after(:create) do |item|
+      item.image.attach(io: File.open('public/images/user_test_image.png'), filename: 'user_test_image.png')
+    end
   end
 end
