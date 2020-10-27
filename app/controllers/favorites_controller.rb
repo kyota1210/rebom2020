adfb66efbf6ee46aca8993f4e5b8a927a1308732
kyota1210 <kytmbmp1210@gmail.com>
2020-10-27 12:30:51 +0900

    Ajaxでお気に入り機能の実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,12 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     favorite = current_user.favorites.create(book_id: params[:book_id])
-    redirect_to root_path
   end
 
   def destroy
-    # book = current_user.favorites.find(params[:id])
+    @book = Book.find(params[:book_id])
     favorite = Favorite.find_by(book_id: params[:book_id], user_id: current_user.id)
     favorite.destroy
-    redirect_to root_path
   end
 end
