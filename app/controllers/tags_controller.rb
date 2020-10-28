b3a9e2d33b5aa33461aef6ed3264aaa8a5ec83be
kyota1210 <kytmbmp1210@gmail.com>
2020-10-28 12:45:52 +0900

    フロントレイアウトの修正

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def index
-    @tags = Tag.all
+    @tags = Tag.all.order('created_at DESC')
     @tag = Tag.find(params[:tag_id])
     @books = @tag.books
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'favorites/create'
-  get 'favorites/destroy'
   devise_for :users
   root to:  'books#index'
   resources :users, only: [:show, :edit, :update]
   resources :books, shallow: true do
     resources :posts, only: [:new, :create, :show, :edit, :update, :destroy]
-    resource :favorites, only: %i[create destroy]
+    resource :favorites, only: [:create, :destroy]
     collection do
       get :favorites
     end
