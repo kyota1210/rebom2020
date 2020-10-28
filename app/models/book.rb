23aa25a5e3bb57cff9a53bb3b69048524c6e3a91
kyota1210 <kytmbmp1210@gmail.com>
2020-10-29 07:34:04 +0900

    タグ付け機能のフロント修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  has_many :posts, dependent: :destroy
+  has_many :posts
   has_many :book_tag_relations, dependent: :destroy
   has_many :tags, through: :book_tag_relations
   has_many :favorites, dependent: :destroy
