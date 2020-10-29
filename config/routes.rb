8849bd7bfd6bc1808602e94050f01178bf2dffaa
kyota1210 <kytmbmp1210@gmail.com>
2020-10-29 17:46:02 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to:  'books#index'
   resources :users, only: [:show, :edit, :update]
-  resources :books, shallow: true do
+  resources :books do
     resources :posts, only: [:new, :create, :show, :edit, :update, :destroy]
     resource :favorites, only: [:create, :destroy]
     collection do
