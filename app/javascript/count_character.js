65444f1af49d5e090257cedf2d461722e56608b6
kyota1210 <kytmbmp1210@gmail.com>
2020-10-30 13:03:40 +0900

    投稿文字数のカウント機能実装

---
+++ app/javascript/count_character.js
@@ -0,0 +1,16 @@
+function count (){
+  const postText = document.getElementById("post_text")
+  postText.addEventListener('keyup', () =>{
+    const countVal = postText.value.length;
+    const charNum = document.getElementById("char-num");
+    charNum.innerHTML = `${countVal}文字`;
+
+    if(countVal > 200){
+      charNum.setAttribute("style", "color:red")
+    }else{
+      charNum.setAttribute("style", "color: #1e2c41")
+    }
+  });
+}
+
+window.addEventListener('load', count);
