e0b42586090d6ee546001d086820d9f4f4238d39
kyota1210 <kytmbmp1210@gmail.com>
2020-11-02 19:01:11 +0900

    本投稿一覧のヘルパーメソッド生成

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,9 @@
 module BooksHelper
+  def book_list(books)
+    html = ''
+    books.each do |book|
+      html += render(partial: 'books', locals: { book: book })
+    end
+  render raw(html)
+  end
 end
