f2764e4727c457fa6129660a82d91ac6e5222b4f
kyota1210 <kytmbmp1210@gmail.com>
2020-11-04 18:11:02 +0900

    出品昨日のサーバーサイド実装

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -3,6 +3,25 @@ before_action :authenticate_user!, only: [:new]
 
   def new
     @book = Book.find(params[:id])
-    @sale = Sale.new
+    @book_sale = BookSale.new
+  end
+
+  def create
+    @book = Book.find(params[:book_id])
+    @book_sale = BookSale.new(sale_params)
+    if @book_sale.valid?
+      @book.sell = :sell
+      @book.save
+      @book_sale.save
+      redirect_to "/users/#{@book.user.id}"
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def sale_params
+    params.require(:book_sale).permit(:sell, :status, :transfer_fee, :price).merge(book_id: params[:book_id])
   end
 end
