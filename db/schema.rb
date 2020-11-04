2cf5b406b062433ff2c99b21bada8c9e55219c70
kyota1210 <kytmbmp1210@gmail.com>
2020-11-04 18:10:09 +0900

    マイグレーションにsellカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_024841) do
+ActiveRecord::Schema.define(version: 2020_11_04_005144) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_11_03_024841) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "sell", default: false, null: false
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
