294232918b25bd06dfbc4d6996bc25575d78257b
kyota1210 <kytmbmp1210@gmail.com>
2020-11-05 16:10:45 +0900

    フォームオブジェクトの廃止

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -5,4 +5,11 @@ class Sale < ApplicationRecord
   belongs_to_active_hash :status
   belongs_to_active_hash :transfer_fee
 
+  PRICE_REGEX = /\A[0-9]+\z/.freeze
+  with_options presence: true do
+    validates :status_id,       numericality: { other_than: 0, message: 'が選択されていません' }
+    validates :transfer_fee_id, numericality: { other_than: 0, message: 'が選択されていません' }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than: 10_000_000, message: 'は¥300~9,999,999で入力してください' },
+                      format: { with: PRICE_REGEX, message: 'は半角数字で入力してください' }
+  end
 end
